1641a520f951bc9578121113b5d7d6120ede655b
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-03 21:01:54 -0500

    listo

---
+++ factorial-break.js
@@ -0,0 +1,16 @@
+//factorial-break.js -> Este archivo debe calcular el factorial 
+//de 10 utilizando 
+//un bucle while, una bifurcación if y una sentencia break
+let n=10
+let f=n
+
+while(n>1){
+    f=f*(n-1)
+    let control=11
+    n--
+   
+   
+    
+   
+}
+console.log(f)
